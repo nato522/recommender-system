e686d59235db03b4cde2d631e5d2d24d416ac55f
Ruxandra22 <ruxandra22.osman@gmail.com>
2019-11-23 20:08:45 +0100

    calculate the pearson matrix

--- recommender_system.py
+++ recommender_system.py
@@ -1,3 +1,4 @@
+import itertools
 import random
 import numpy as np
 import math
@@ -48,28 +49,6 @@ def empty_random(matrix, fraction):
     return matrix
 
 
-# helper function
-def print_matrix(matrix):
-    i_aux = 0
-    j_aux = 0
-    for i_aux in range(LINES):
-        for j_aux in range(COLUMNS):
-            print(matrix[i_aux][j_aux], end=" ")
-        print()
-
-
-# helper function to check that 25% of the matrix is with the value 0
-def count_zeros(matrix):
-    i_aux = 0
-    j_aux = 0
-    count = 0
-    for i_aux in range(LINES):
-        for j_aux in range(COLUMNS):
-            if matrix[i_aux][j_aux] == 0:
-                count += 1
-    print(count)
-
-
 def get_average(arr):
     arr_total = sum(arr)
     arr_length = np.count_nonzero(arr)
@@ -77,21 +56,20 @@ def get_average(arr):
 
 
 def normalize_matrix(matrix):
+    aux_matrix = matrix.copy()
     row = 0
     column = 0
     for row in range(LINES):
-        arr = matrix[row]
+        arr = aux_matrix[row]
         avg = get_average(arr)
         for column in range(COLUMNS):
-            elem = matrix[row][column]
+            elem = aux_matrix[row][column]
             if elem != 0.0:
-                matrix[row][column] = matrix[row][column] - avg
-    return matrix
+                aux_matrix[row][column] = aux_matrix[row][column] - avg
+    return aux_matrix
 
 
-def pearson():
-    x = [5, 1, 0, 2, 2]
-    y = [1, 5, 2, 5, 5]
+def pearson(x, y):
 
     x_mean = get_average(x)
     y_mean = get_average(y)
@@ -109,17 +87,55 @@ def pearson():
     denominator = math.sqrt(sum_den_x * sum_den_y)
 
     pearson_result = numerator / denominator
-    return pearson_result;
+    return pearson_result
+
+
+def calculate_pearson_matrix(matrix):
+    pearson_matrix = [[0 for x in range(LINES)] for y in range(LINES)]
+
+    lines = len(matrix)
+    columns = len(matrix[0])
+
+    # get all the combinations of rows from the matrix
+    indexes_list = list(itertools.product(range(lines), repeat=2))
+    for ele in range(0, len(indexes_list)):
+        i = indexes_list[ele][0]
+        j = indexes_list[ele][1]
+        pearson_matrix[i][j] = pearson(matrix[i], matrix[j])
+
+    return pearson_matrix
+
+
+# helper function
+def print_matrix(matrix):
+    i_aux = 0
+    j_aux = 0
+    for i_aux in range(len(matrix)):
+        for j_aux in range(len(matrix[0])):
+            print(matrix[i_aux][j_aux], end=" ")
+        print()
+
+
+# helper function to check that 25% of the matrix is with the value 0
+def count_zeros(matrix):
+    i_aux = 0
+    j_aux = 0
+    count = 0
+    for i_aux in range(len(matrix)):
+        for j_aux in range(len(matrix[0])):
+            if matrix[i_aux][j_aux] == 1.0:
+                count += 1
+    return count
 
 
 def main():
     movie_matrix = read_matrix()
     movie_matrix = empty_random(movie_matrix, 0.25)
-    print_matrix(movie_matrix)
-    count_zeros(movie_matrix)
     norm_matrix = normalize_matrix(movie_matrix)
-    print_matrix(norm_matrix)
-    print(pearson())
+
+    pearson_matrix = calculate_pearson_matrix(norm_matrix)
+    print_matrix(pearson_matrix)
+    print(count_zeros(pearson_matrix))
 
 
 main()
